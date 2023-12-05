55e62fa85fdec6b74385286d9c24c59e8781077a
Ekinsley2002 <146798945+Ekinsley2002@users.noreply.github.com>
2023-12-04 18:28:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
-function validateForm() {
+function validateInfo() {
     let isValid = true;
     let errorMessage = "";
 
     // Validating empty fields
-    if (document.getElementById('firstName').value == "") {
-        errorMessage += "First Name is required.\n";
+    if (document.getElementById('first_name').value == "") {
+        errorMessage += "Please enter your first name.\n";
         isValid = false;
     }
-    if (document.getElementById('lastName').value == "") {
-        errorMessage += "Last Name is required.\n";
+    if (document.getElementById('last_name').value == "") {
+        errorMessage += "Please enter your last name.\n";
+        isValid = false;
+    
+
+    if (document.getElementById('confirm_password').value == "") {
+        errorMessage += "Please confirm your password.\n";
         isValid = false;
     }
+
     if (document.getElementById('email').value == "") {
-        errorMessage += "Email is required.\n";
+        errorMessage += "Please enter your email.\n";
         isValid = false;
+        
     }
+        
     if (document.getElementById('password').value == "") {
         errorMessage += "Password is required.\n";
         isValid = false;
     }
-    if (document.getElementById('confirmPassword').value == "") {
-        errorMessage += "Confirm Password is required.\n";
-        isValid = false;
-    }
+    
 
     // Validating email format
     var email = document.getElementById('email').value;
+        
     var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        
     if (email && !emailRegex.test(email)) {
-        errorMessage += "Invalid email format.\n";
+        errorMessage += "Invalid email.\n";
         isValid = false;
     }
 
     // Checking if passwords match
-    if (document.getElementById('password').value != document.getElementById('confirmPassword').value) {
+    if (document.getElementById('password').value != document.getElementById('confirm_password').value) {
         errorMessage += "Passwords do not match.\n";
         isValid = false;
     }
@@ -41,10 +48,13 @@ function validateForm() {
     // Display error message or success message
     if (!isValid) {
         alert("Please correct the following errors:\n" + errorMessage);
-    } else {
+    } 
+        
+    else {
         alert("All information is valid.");
     }
 
     return isValid;
+        
 }
 
