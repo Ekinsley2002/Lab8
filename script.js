74af1fcc1330822e9d315f2ee4b37ddded6ffced
Ekinsley2002 <146798945+Ekinsley2002@users.noreply.github.com>
2023-12-04 19:08:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,45 @@
 function validateInfo() {
-    
     let correct = true;
     let errorMes = "";
-    
+
     const first_name_validation = DOMPurify.sanitize(document.getElementById('first_name').value);
     const last_name_validation = DOMPurify.sanitize(document.getElementById('last_name').value);
     const email_validation = DOMPurify.sanitize(document.getElementById('email_validation').value);
 
-    if (document.getElementById('first_name_validation').value == "") {
+    if (first_name_validation === "") {
         errorMes += "Please enter your first name.\n";
         correct = false;
     }
-    if (document.getElementById('last_name_validation').value == "") {
+    if (last_name_validation === "") {
         errorMes += "Please enter your last name.\n";
         correct = false;
     }
 
-    if (document.getElementById('confirm_password').value == "") {
+    if (document.getElementById('confirm_password').value === "") {
         errorMes += "Please confirm your password.\n";
         correct = false;
     }
 
-    if (document.getElementById('email_validation').value == "") {
+    if (email_validation === "") {
         errorMes += "Please enter your email.\n";
         correct = false;
-    
     }
-        
-    if (document.getElementById('password').value == "") {
+
+    if (document.getElementById('password').value === "") {
         errorMes += "Password is required.\n";
         correct = false;
     }
-    
 
     // Validating email format
-    var email = document.getElementById('email').value;
-        
-    var emailIndex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        
-    if (email && !emailIndex.test(email)) {
+    const emailIndex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (email_validation && !emailIndex.test(email_validation)) {
         errorMes += "Invalid email.\n";
         correct = false;
     }
 
     // Checking if passwords match
-    if (document.getElementById('password').value != document.getElementById('confirm_password').value) {
+    if (document.getElementById('password').value !== document.getElementById('confirm_password').value) {
         errorMes += "Password needs to match confirm password.\n";
         correct = false;
     }
@@ -53,12 +48,9 @@ function validateInfo() {
     if (!correct) {
         alert("Please correct the following errors:\n" + errorMes);
     } 
-        
     else {
         alert("All information is valid.");
     }
 
     return correct;
-        
 }
-
