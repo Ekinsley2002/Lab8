84804f118f25da761e575d1a956e75e44e92c1a1
Ekinsley2002 <146798945+Ekinsley2002@users.noreply.github.com>
2023-12-04 18:13:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 function validateForm() {
+    let isValid = true;
+    let errorMessage = "";
+
     // Validating empty fields
-    if (document.getElementById('firstName').value == "" ||
-        document.getElementById('lastName').value == "" ||
-        document.getElementById('email').value == "" ||
-        document.getElementById('password').value == "" ||
-        document.getElementById('confirmPassword').value == "") {
-        alert("All fields must be filled out");
-        return false;
+    if (document.getElementById('firstName').value == "") {
+        errorMessage += "First Name is required.\n";
+        isValid = false;
+    }
+    if (document.getElementById('lastName').value == "") {
+        errorMessage += "Last Name is required.\n";
+        isValid = false;
+    }
+    if (document.getElementById('email').value == "") {
+        errorMessage += "Email is required.\n";
+        isValid = false;
+    }
+    if (document.getElementById('password').value == "") {
+        errorMessage += "Password is required.\n";
+        isValid = false;
+    }
+    if (document.getElementById('confirmPassword').value == "") {
+        errorMessage += "Confirm Password is required.\n";
+        isValid = false;
     }
 
     // Validating email format
     var email = document.getElementById('email').value;
     var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (!emailRegex.test(email)) {
-        alert("Invalid email format");
-        return false;
+    if (email && !emailRegex.test(email)) {
+        errorMessage += "Invalid email format.\n";
+        isValid = false;
     }
 
     // Checking if passwords match
     if (document.getElementById('password').value != document.getElementById('confirmPassword').value) {
-        alert("Passwords do not match");
-        return false;
+        errorMessage += "Passwords do not match.\n";
+        isValid = false;
     }
 
-    return true;
+    // Display error message or success message
+    if (!isValid) {
+        alert("Please correct the following errors:\n" + errorMessage);
+    } else {
+        alert("All information is valid.");
+    }
+
+    return isValid;
 }
+
